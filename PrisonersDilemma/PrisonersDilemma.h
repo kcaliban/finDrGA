523eb43fbcded62f820eca18a73c9ee2e20bf985
kcaliban <29677855+kcaliban@users.noreply.github.com>
2019-09-13 16:12:08 +0200

    Cleanup

--- PrisonersDilemma/PrisonersDilemma.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRISMAIN
-#define PRISMAIN
-#include "PrisonersDilemmaGenoType.h"
-#include "PrisonersDilemmaGenome.h"
-#include "PrisonersDilemmaFitnessFunc.h"
-#include "../lib/GenAlgInst.h"
-#include <stdlib.h>
-
-#endif
